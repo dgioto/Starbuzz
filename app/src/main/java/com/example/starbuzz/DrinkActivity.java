99c7f0d1709bf51e7386bec800d4c7b3cfddbcb9
Alexander Tih <dgioto@gmail.com>
2021-05-10 09:19:07 +0300

    изменение DrinkActivity для использованиябазы данных Starbuzz

--- app/src/main/java/com/example/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/example/starbuzz/DrinkActivity.java
@@ -2,13 +2,18 @@ package com.example.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
 
+    //идентификатор напитка, выбранного пользователем
     public static final String EXTRA_DRINKID = "drinkId";
 
     @Override
@@ -18,19 +23,47 @@ public class DrinkActivity extends AppCompatActivity {
 
         //получить напиток из данный интента
         int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
-        Drink drink = Drink.drinks[drinkId];
 
-        //Заполнение названия напитка
-        TextView name = (TextView) findViewById(R.id.name);
-        name.setText(drink.getName());
+        //создание курсора
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            //создаем курсор для получегия из таблицы DRINK  столбцов NAME, DESCRIPTION,
+            //IMAGE_RESOURCE_ID тех записей у которых значение _id равно drinkId
+            Cursor cursor = db.query("DRINK",
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkId)},
+                    null, null, null);
 
-        //Заполнить описание напитка
-        TextView description = (TextView) findViewById(R.id.description);
-        description.setText(drink.getDescription());
+            //переход к первой записи в курсоре
+            if (cursor.moveToFirst()){
 
-        //Заполнить изображения напитка
-        ImageView photo = (ImageView) findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getName());
+                //получение данных напитка из курсора
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                //Заполнение названия напитка
+                TextView name = findViewById(R.id.name);
+                name.setText(nameText);
+
+                //Заполнить описание напитка
+                TextView description = findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                //Заполнить изображения напитка
+                ImageView photo = findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+
+            }
+
+        } catch (SQLException e){
+            Toast toast = Toast.makeText(this,
+                    "Database unavailable",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
