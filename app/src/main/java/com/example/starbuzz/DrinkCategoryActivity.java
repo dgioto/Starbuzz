4838b66dbba45eb1aef6880e9bc1fc41d851df11
Alexander Tih <dgioto@gmail.com>
2021-05-11 23:12:29 +0300

    изменение DrinkCategoryActivity для использования базы данных Starbuzz

--- app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
@@ -3,27 +3,48 @@ package com.example.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
 
-        //добавление спискового представления с помощью адаптера массивов
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks
-        );
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            //создаем адаптер курсора
+            //Связать содержимое столбца сNAME с текстом в ListView
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            //адаптер связывается с ListView
+            listDrinks.setAdapter(listAdapter);
+        } catch (SQLException e){
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
         //создание слушателя
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
@@ -41,5 +62,12 @@ public class DrinkCategoryActivity extends AppCompatActivity {
 
         //Назначение слушателя для спискового представления
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
