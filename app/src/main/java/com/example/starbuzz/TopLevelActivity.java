6fa4900c48c24433550ce8db83833c8eccdf6d1a
Alexander Tih <dgioto@gmail.com>
2021-05-13 16:43:48 +0300

    переработка  TopActivity

--- app/src/main/java/com/example/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/starbuzz/TopLevelActivity.java
@@ -14,12 +14,20 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        
+        //вызов нового метода
+        setupOptionsListView();
+    }
 
+    private void setupOptionsListView() {
         //создание объект слушателя
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
                 if (position == 0){
+                    //Если пользователь выбрал Drink в списковом представлении list_options,
+                    // открыть активность DrinkCategoryActivity
                     Intent intent = new Intent(
                             TopLevelActivity.this,
                             DrinkCategoryActivity.class);
